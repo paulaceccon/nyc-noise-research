cce0a7c12d406d19dec3c64de4602379686ed8f6
Paula Ceccon <pceccon@172-16-26-40.DYNAPOOL.NYU.EDU>
2015-12-15 14:32:20 -0500

    Getting roads and 311 info.

--- Scripts/noiseInference.py
+++ Scripts/noiseInference.py
@@ -1,10 +1,12 @@
 import urllib2, json, csv
 import numpy
+import requests
 
 from shapely.geometry import shape, Point
 from rtree import index
+from datetime import datetime, date, timedelta
 
-
+###################----------- Utils -----------###################
 def readJson(url):
     """
     Returns a json file specified in @url.
@@ -21,6 +23,7 @@ def readCSV(url):
     return csv.DictReader(response, delimiter=',')
 
 
+###################----------- Collection Data -----------###################
 def getRegions():
     """
     Returns a dictionary formed by the id of a region and its coordinates.
@@ -30,17 +33,34 @@ def getRegions():
     url = "https://nycdatastables.s3.amazonaws.com/2013-08-19T18:22:23.125Z/community-districts-polygon.geojson"
     data = readJson(url)
     for district in data['features']:
+    	# (long, lat)
         dict[district['id']] = district['geometry']
 
     return dict
-
+    
+    
+def getRoadsNodes():
+	"""
+	Reads the nodes of the NYC roads, returning a list of tuples of (long, lat).
+	"""
+	nodes = []
+	
+	f = open('../Resources/road-network.txt', 'r')
+	nodes_number = int(f.readline().split()[0])
+	print "Nodes:", nodes_number
+	for line in range(nodes_number):
+		lat_long = f.readline().split()
+		nodes.append((float(lat_long[1]), float(lat_long[0])))
+	f.close()
+	
+	return nodes
+	
 
 def getPOIs():
     """
     Returns a list of tuples of POIs lat/long coordinates.
     """
-    urls = [
-        	"https://nycdatastables.s3.amazonaws.com/2013-06-04T18:02:56.019Z/museums-and-galleries-results.csv",
+    urls = ["https://nycdatastables.s3.amazonaws.com/2013-06-04T18:02:56.019Z/museums-and-galleries-results.csv",
             "https://nycdatastables.s3.amazonaws.com/2013-12-16T21:49:55.716Z/nyc-parking-facilities-results.csv",
             "https://nycdatastables.s3.amazonaws.com/2013-06-20T16:06:05.136Z/mapped-in-ny-companies-results.csv",
             "https://nycdatastables.s3.amazonaws.com/2013-06-11T18:59:27.269Z/nyc-public-school-locations-results.csv",
@@ -56,8 +76,7 @@ def getPOIs():
             "https://nycdatastables.s3.amazonaws.com/2013-10-18T21:14:52.348Z/nyc-grocery-stores-final.csv",
             "https://nycdatastables.s3.amazonaws.com/2013-06-18T14:29:37.626Z/subway-entrances-results.csv",
             "https://nycdatastables.s3.amazonaws.com/2013-06-04T17:58:59.335Z/map-of-monuments-results.csv",
-            "https://nycdatastables.s3.amazonaws.com/2013-06-18T20:17:34.010Z/nyc-landmarks-results.csv"
-            ]
+            "https://nycdatastables.s3.amazonaws.com/2013-06-18T20:17:34.010Z/nyc-landmarks-results.csv"]
 
     POIs = []
 
@@ -69,57 +88,104 @@ def getPOIs():
             longitude = line.get('longitude', None)
             if latitude is not None and longitude is not None:
                 POIs.append((float(longitude), float(latitude)))
-                # POIs.append((float(latitude), float(longitude)))
+
     return POIs
-
-
-def POIsInRegion(regions, POIs):
-    """
-    Returns a dictionary formed by the id of a region and the number of POIs that falls in
-    this region.
-    """
-    dict = {}
     
-    polygons = []
-    # Populate R-tree index with bounds of polygons
-    idx = index.Index()
-    for pos, poly in enumerate(regions):
-    	dict[poly] = 0
-    	polygon = shape(regions[poly])
-    	polygons.append(polygon)
-        idx.insert(pos, polygon.bounds)
-        
-    for i, p in enumerate(POIs):
+
+def get311NoiseComplaints():
+    # Yesterday's date
+	date = str(datetime.date(datetime.now()) - timedelta(1))
+	
+	query_string = "http://data.cityofnewyork.us/resource/fhrw-4uyv.json"
+	query_string += "?"                                                   
+	query_string += "$where="                                            
+	query_string += "(complaint_type like '%Noise%')"
+	query_string += " AND "
+	query_string += "(created_date>='" + date + "')"
+	query_string += "&$group=descriptor,latitude,longitude"
+	query_string += "&$select=descriptor,latitude,longitude"
+	
+	print query_string
+	result = requests.get(query_string).json()
+    
+    
+###################----------- Data Per Region -----------###################
+def pointInPolygon(polyDict, points):
+	"""
+	Given a dictionary (@polyDict) of id : polygon and a list of points (as tuples),
+	returns a dictionary id : number of points inside polygon.
+	"""
+	dict = {}
+	
+	polygons = []
+	# Populate R-tree index with bounds of polygons
+	idx = index.Index()
+	for pos, poly in enumerate(polyDict):
+		dict[poly] = 0
+		polygon = shape(polyDict[poly])
+		polygons.append(polygon)
+		idx.insert(pos, polygon.bounds)
+		
+	for i, p in enumerate(points):
   		point = Point(p[0], p[1])
   		# Iterate through spatial index
   		for j in idx.intersection(point.coords[0]):
   			if point.within(polygons[j]):
   				dict[j] += 1
-  	
+  				
   	# Not optmized
-#     for key, value in regions.iteritems():
+#     for key, value in polyDict.iteritems():
 #         dict[key] = 0
 #         polygon = shape(value)
 #         for p in POIs:
 #             point = Point(p[0], p[1])
-#             # print point.wkt
 #             if polygon.contains(point):
-#                 dict[key] += 1
+#                 dict[key] += 1  				
+  	
+  	return dict
 
-    return dict
+def POIsPerRegion(regions, POIs):
+    """
+    Returns a dictionary formed by the id of a region and the number of POIs that falls in
+    this region.
+    @regions is a dictionary in the form id : region shape
+    @POIs is a list of tuples (long, lat)
+    """
+    return pointInPolygon(regions, POIs)
+
+        
+def roadsNodesPerRegion(regions, nodes):
+	"""
+	Returns a dictionary formed by the id of a region and the number of nodes that falls in
+	this region.
+	@regions is a dictionary in the form id : region shape
+	@nodes is a list of tuples (long, lat)
+	"""
+	return pointInPolygon(regions, nodes)
 
 
 if __name__ == '__main__':
     # Geographical Features
     regions_bbox = getRegions()
     regions_number = len(regions_bbox)
-    print "Regions: ", regions_number
+    print "Regions:", regions_number
 
-    print "Reading POIs..."
-    POIs = getPOIs()
-    print len(POIs)
-    print "Done Reading POIs"
-
-    print "Calculating POIs per Region"
-    POIsPerRegion = POIsInRegion(regions_bbox, POIs)
-    print POIsPerRegion
+    # print "Reading POIs..."
+#     POIs = getPOIs()
+#     print "POIs:", len(POIs)
+#     print "Done Reading POIs"
+# 
+#     print "Calculating POIs per Region"
+#     POIsPerRegion = POIsPerRegion(regions_bbox, POIs)
+#     print POIsPerRegion
+#     
+#     nodes = getRoadsNodes()
+#     print "Nodes:", len(nodes)
+#     
+#     print "Calculating nodes per Region"
+#     nodesPerRegion = roadsNodesPerRegion(regions_bbox, nodes)
+#     print nodesPerRegion
+    
+    print "Getting noise complaints"
+    get311NoiseComplaints()
+    
